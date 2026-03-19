131e7cbc20d4cd5dabaac6a1a1ff537f44dfb1ef
Zitha Makasi <zithamakasi179@gmail.com>
2026-03-19 22:44:32 +0200

    Moved content into the folder

---
+++ script.js
@@ -0,0 +1,11 @@
+function makeItdark() {
+  let body = document.querySelector("body");
+
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+  } else {
+    body.classList.add("dark");
+  }
+}
+let themeButton = document.querySelector(".theme-button");
+themeButton.addEventListener("click", makeItdark);
